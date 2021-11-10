06d7f9d5165587930429ba91134a1dabf26e5ead
Manan Patel <73080454+Manan-dev@users.noreply.github.com>
2021-11-10 23:24:19 +0000

    Add html and js part of project

---
+++ script.js
@@ -0,0 +1,20 @@
+const button = document.getElementById("btn");
+const pTag = document.getElementById("pTag");
+button.addEventListener('click', apiCall);
+
+async function apiCall(){
+  const options = {
+    method: 'GET',
+    headers: {
+      "Accept" : "application/json"
+    }
+  }
+  try {
+    let response = await fetch("https://icanhazdadjoke.com/",options);
+    let responseAsJson = await response.json();
+    pTag.innerText = responseAsJson.joke;
+  } catch(error){
+    console.log(error.message);
+  }
+}
+
