94b3638ee8efe2ee2fed67c5448f06438de14d80
fzbowers <93776953+fzbowers@users.noreply.github.com>
2021-11-12 16:46:20 +0000

    Added gif

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ const button = document.getElementById("btn");
 const pTag = document.getElementById("pTag");
 button.addEventListener('click', apiCall);
 
-async function apiCall(){
+async function apiCall() {
   const options = {
     method: 'GET',
     headers: {
-      "Accept" : "application/json"
+      "Accept": "application/json"
     }
   }
   try {
-    let response = await fetch("https://icanhazdadjoke.com/",options);
+    let response = await fetch("https://icanhazdadjoke.com/", options);
     let responseAsJson = await response.json();
     pTag.innerText = responseAsJson.joke;
-  } catch(error){
+  } catch (error) {
     console.log(error.message);
   }
 }
